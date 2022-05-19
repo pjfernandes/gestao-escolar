9ce6c0a4d92b72169f26fd5d91cf657332c7f0d7
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-05-19 10:56:55 -0300

    address fields added to form

---
+++ script.js
@@ -0,0 +1,11 @@
+// const inputs = document.querySelectorAll("input");
+
+// inputs.forEach(element => {
+//   // element.addEventListener("mouseenter", (event => {
+//     if (element.value.length > 0) {
+//       element.style.backgroundColor="lightgray";
+//     } else {
+//       element.style.backgroundColor = "";
+//     }
+//   }));
+// });
